956b0be3c5c626ffe0661a67194172103559c2b6
legerM <mickael.leger@le-campus-numerique.fr>
2020-01-23 15:57:01 +0100

    mon interface utilisateuravec terminaltables en plus et une reorganisation du code par extraction de method

--- repertoire_ui.py
+++ repertoire_ui.py
@@ -1,55 +1,70 @@
-from repertoire_action import *
+import repertoire_action as action
+from terminaltables import AsciiTable
 
-repertoire = [ {"nom": "george", "numero": "32514564", "mail": "@"},
-                {"nom": "bob", "numero": "6", "mail": "@"},
-                {"nom": "gnu", "numero": "5645654", "mail": "@"}]
+def afficher_tableau(repertoire):
+    tableau = [["nom", "numeros", "adresse"]]
+    liste_contacts = (action.lister_contacts(repertoire))
+    for contacts in liste_contacts:
+        tableau.append([contacts["nom"], contacts["telephone"], contacts["adresse"]])
+    print(AsciiTable(tableau).table)
+
+
+def afficher_tout_les_contacts(repertoire):
+    afficher_tableau(repertoire)
+
+
+def supprimer_personne():
+    name = input("\nquel contact voulez vous supprimer ?")
+    if action.supprimer_personne(repertoire, name):
+        print("le contact a été supprimer")
+        afficher_tout_les_contacts(repertoire)
+    else:
+        print("Réessaye")
+
+
+def ajouter_personne():
+    nom = input("\nquel est le nom du contact ?")
+    telephone = input("quel est le numero ?")
+    adresse = input("quelle est l'adresse ?")
+    added = action.ajouter_personne(repertoire, nom, telephone, adresse)
+    if added:
+        print("\nle contact à été ajouter")
+        afficher_tout_les_contacts(repertoire)
+    else:
+        print("le contact existe déja")
+
+def afficher_recherche(resultats_recherche):
+    print(resultats_recherche)
 
 
 
 
-def afficher_repertoire (repertoire):
-    print(repertoire)
-
+def rechercher_personne():
+    recherche = input("\nveuillez entrer le nom du contact :")
+    resultats_recherche = action.recherche_personnes(repertoire, recherche)
+    if resultats_recherche:
+        afficher_recherche(resultats_recherche)
+    else:
+        print("aucun contact a ce nom")
 
 
 while True:
-    choix_utilisateur = input("\nPress L pour lister , A pour ajouter et S pour supprimer un contact ou R pour rechercher un contact :").upper()
+    repertoire = action.get_rep()
+    choix_utilisateur = input("\nPress L pour lister , A pour ajouter et S pour supprimer un contact ou R pour "
+                              "rechercher un contact :").upper()
 
     if choix_utilisateur == "L":
         print("\nvoici les contacts")
-        afficher_repertoire(repertoire)
+        afficher_tout_les_contacts(repertoire)
 
     elif choix_utilisateur == "S":
-        name=input("\nquel contact voulez vous supprimer ?")
-        if supprimer_contact(repertoire, name):
-            print("le contact a été supprimer")
-        else:
-            print("Réessaye")
+        supprimer_personne()
 
     elif choix_utilisateur == "A":
-        nom = input("\nquel est le nom du contact ?")
-        numero = input("quel est le numero ?")
-        adresse = input("quelle est l'adresse ?")
-        added=ajouter_contact(repertoire, nom, numero, adresse)
-        if added :
-            print("\nle contact à été ajouter")
-        else:
-            print("le contact existe déja")
+        ajouter_personne()
 
-
-
-
-# la recherche de contact qui utilise plusieur fonctions , la recherche du contact , et l'afichage du repertoire
     elif choix_utilisateur == "R":
-        recherche = input("\nveuillez entrer le nom du contact :")
-        resultats_recherche= recherche_contact(repertoire, recherche)
-        if resultats_recherche:
-            afficher_repertoire(resultats_recherche)
-        else :
-            print("aucun contact a ce nom")
+        rechercher_personne()
 
-
-
-
-
-
+    elif choix_utilisateur =="Q":
+        break
